9eff7d10cb95417aa4bc8b2c226a7e7b83919c3b
Saber___Dev <saber.esmaili1414@gmail.com>
2025-06-07 13:11:45 +0330

    feat: add new cofee project

--- pages/menu/index.jsx
+++ pages/menu/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Index = () => {
   return (
     <>
-      <h1>Home Page</h1>
+        <h1>Menu Page</h1> 
     </>
   )
 }
---
+++ pages/product/[id].jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <>
+        <h1>Product Info</h1> 
+    </>
+  )
+}
+
+export default Product
